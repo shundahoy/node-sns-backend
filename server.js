3dec08db220ba3bec6272c20014fbe3890448163
shundahoy <shundahoy@gmail.com>
2022-09-12 22:43:13 +0900

    first commit

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require("express");
 
 const app = express();
 const PORT = 5000;
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("hello wxpress");
-});
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("database is connect");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(PORT, () => {
   console.log("sssssssss");
