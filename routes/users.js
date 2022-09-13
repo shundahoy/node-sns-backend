61a26be770167b8cfd657666e60821d9dc2d7b9b
shundahoy <shundahoy@gmail.com>
2022-09-13 18:04:52 +0900

    update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("users");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    const user = await newUser.save();
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
