b591483f41c063afd95dbadab0c862db3528e121
shundahoy <shundahoy@gmail.com>
2022-09-14 15:06:49 +0900

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,39 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("auth");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    const user = await newUser.save();
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (!user) return res.status(404).send("ユーザーが見つかりません");
+
+    const vailedPassword = req.body.password === user.password;
+
+    if (!vailedPassword) return res.status(400).json("パスワードが違います");
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,35 @@ const router = require("express").Router();
 
 const User = require("../models/User");
 
-router.post("/register", async (req, res) => {
-  try {
-    const newUser = await new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findOneAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("ユーザー情報が更新されました");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を更新できます");
+  }
+});
 
-    const user = await newUser.save();
-
-    return res.status(200).json(user);
-  } catch (error) {
-    return res.status(500).json(err);
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザー情報が消去されました");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を消去できます");
   }
 });
 
