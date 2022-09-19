5463540fdcd4ed45c43bc516cca1381b722d1ec8
shundahoy <shundahoy@gmail.com>
2022-09-19 13:59:54 +0900

    update

--- routes/posts.js
+++ routes/posts.js
@@ -11,4 +11,20 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const post = new Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("成功");
+    } else {
+      return res.status(403).json("ほかの人の投稿です");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
