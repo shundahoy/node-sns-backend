8573247434247a037cbf4f671851e4692b83f98e
shundahoy <shundahoy@gmail.com>
2022-09-22 18:46:42 +0900

    update

--- routes/posts.js
+++ routes/posts.js
@@ -74,9 +74,9 @@ router.put("/:id/like", async (req, res) => {
   }
 });
 
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     //
     const friendPosts = await Promise.all(
@@ -85,6 +85,17 @@ router.get("/timeline/all", async (req, res) => {
       })
     );
     return res.status(200).json(userPosts.concat(...friendPosts));
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get("/profile/:username", async (req, res) => {
+  try {
+    const currentUser = await User.findOne({ username: req.params.username });
+    const userPosts = await Post.find({ userId: currentUser._id });
+
+    return res.status(200).json(userPosts);
   } catch (error) {
     return res.status(500).json(error);
   }
